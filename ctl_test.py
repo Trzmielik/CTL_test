f3d7794645eaced9e47188a594bab2cbd9ee583c
trzmie <orgimi@pm.me>
2022-11-22 14:58:12 +0100

    Added making histograms.

--- ctl_test.py
+++ ctl_test.py
@@ -1,4 +1,5 @@
 from random import random
+import matplotlib.pyplot as plt
 
 def my_distribution(x,y):
     if x > -1.25 and x < 1.25:
@@ -9,6 +10,7 @@ def my_distribution(x,y):
         if y<=0.25: 
             return x
         else: return -1
+
 
 def mean(values):
     m=0
@@ -30,3 +32,25 @@ def list_of_random_in_distribution(n):
             print(val)
         n=n-1
     return lst
+
+
+sequence=list_of_random_in_distribution(10000)
+means_2=[]
+means_5=[]
+means_10=[]
+n=0
+while n<len(sequence):
+    means_2.append(mean(sequence[n:n+1]))
+    n=n+2
+n=0
+while n<len(sequence):
+    means_5.append(mean(sequence[n:n+4]))
+    n=n+5
+n=0
+while n<len(sequence):
+    means_10.append(mean(sequence[n:n+9]))
+    n=n+10
+plt.hist(means_2)
+plt.hist(means_5)
+plt.hist(means_10)
+plt.show()
