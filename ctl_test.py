51380b3e4bebf60c0a23b486af111912026880f8
trzmie <orgimi@pm.me>
2022-11-22 17:39:48 +0100

    Increased histogram readability.

--- ctl_test.py
+++ ctl_test.py
@@ -1,6 +1,8 @@
 from random import random
 import matplotlib.pyplot as plt
 
+n_bins=30
+n_values=10000
 def my_distribution(x,y):
     if x > -1.25 and x < 1.25:
         if y<=1:
@@ -33,24 +35,27 @@ def list_of_random_in_distribution(n):
         n=n-1
     return lst
 
-
-sequence=list_of_random_in_distribution(10000)
-means_2=[]
-means_5=[]
-means_10=[]
-n=0
-while n<len(sequence):
-    means_2.append(mean(sequence[n:n+1]))
-    n=n+2
-n=0
-while n<len(sequence):
-    means_5.append(mean(sequence[n:n+4]))
-    n=n+5
-n=0
-while n<len(sequence):
-    means_10.append(mean(sequence[n:n+9]))
-    n=n+10
-plt.hist(means_2)
-plt.hist(means_5)
-plt.hist(means_10)
-plt.show()
+if __name__ == "__main__":
+    n_bits=input("How many bars do you want to have in histogram?")
+    sequence=list_of_random_in_distribution(n_bins*n_values)
+    means_2=[]
+    means_5=[]
+    means_10=[]
+    n=0
+    while n<len(sequence):
+        means_2.append(mean(sequence[n:n+1]))
+        n=n+2
+    n=0
+    while n<len(sequence):
+        means_5.append(mean(sequence[n:n+4]))
+        n=n+5
+    n=0
+    while n<len(sequence):
+        means_10.append(mean(sequence[n:n+9]))
+        n=n+10
+    plt.hist(means_2,label="of 2",bins=n_bins)
+    plt.hist(means_5,label="of 5",bins=n_bins)
+    plt.hist(means_10,label="of 10",bins=n_bins)
+    plt.legend()
+    plt.title("Histograms of means: ")
+    plt.show()
